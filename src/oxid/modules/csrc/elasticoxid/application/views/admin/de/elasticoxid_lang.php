1bf27cc1c7969efc57af3a9c1628df8ea3009251
Mike Bertram <bertram@nexus-netsoft.com>
2016-06-10 08:53:51 +0200

    + ElasticOxid: Language-File

--- src/oxid/modules/csrc/elasticoxid/application/views/admin/de/elasticoxid_lang.php
+++ src/oxid/modules/csrc/elasticoxid/application/views/admin/de/elasticoxid_lang.php
@@ -7,6 +7,19 @@ $aLang = array(
     'SHOP_MODULE_GROUP_main'    => 'Verbindung',
     'SHOP_MODULE_GROUP_systems' => 'Systeme',
 
+    'SHOP_MODULE_sElasticOxidHost'       => 'Host',
+    'SHOP_MODULE_sElasticOxidPort'       => 'Port',
+    'SHOP_MODULE_sElasticOxidPath'       => 'Path',
+    'SHOP_MODULE_sElasticOxidUrl'        => 'Url',
+    'SHOP_MODULE_sElasticOxidProxy'      => 'Proxy',
+    'SHOP_MODULE_sElasticOxidTransport'  => 'Transport',
+    'SHOP_MODULE_sElasticOxidPersistent' => 'Persistent',
+    'SHOP_MODULE_sElasticOxidTimeout'    => 'Timeout',
+    'SHOP_MODULE_sElasticOxidRoundRobin' => 'Round Robin',
+    'SHOP_MODULE_sElasticOxidLog'        => 'Log',
+
+    'SHOP_MODULE_bElasticOxidContent' => 'CMS-Seiten',
+
 );
 /*
 [{ oxmultilang ident="GENERAL_YOUWANTTODELETE" }]
